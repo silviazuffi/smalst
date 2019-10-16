5d2143865c230ec62fc3baaa4af0402fdd2c4616
silviazuffi <silvia.zuffi@tue.mpg.de>
2019-10-16 09:36:39 +0200

    Update smal_torch.py

--- smal_model/smal_torch.py
+++ smal_model/smal_torch.py
@@ -37,21 +37,21 @@ class SMAL(object):
         # Size of mesh [Number of vertices, 3]
         self.size = [self.v_template.shape[0], 3]
         self.num_betas = dd['shapedirs'].shape[-1]
-        # Shape blend shape basis: 6980 x 3 x 10
-        # reshaped to 6980*30 x 10, transposed to 10x6980*3
+        # Shape blend shape basis
+        
         shapedir = np.reshape(
             undo_chumpy(dd['shapedirs']), [-1, self.num_betas]).T
         self.shapedirs = Variable(
             torch.Tensor(shapedir).cuda(device=self.opts.gpu_id), requires_grad=False)
 
-        # Regressor for joint locations given shape - 6890 x 24
+        # Regressor for joint locations given shape 
         self.J_regressor = Variable(
             torch.Tensor(dd['J_regressor'].T.todense()).cuda(device=self.opts.gpu_id),
             requires_grad=False)
 
-        # Pose blend shape basis: 6890 x 3 x 207, reshaped to 6890*30 x 207
+        # Pose blend shape basis
         num_pose_basis = dd['posedirs'].shape[-1]
-        # 207 x 20670
+        
         posedirs = np.reshape(
             undo_chumpy(dd['posedirs']), [-1, num_pose_basis]).T
         self.posedirs = Variable(
@@ -73,7 +73,7 @@ class SMAL(object):
             nBetas = 0
 
         # 1. Add shape blend shapes
-        # (N x 10) x (10 x 3880*3) = N x 3889 x 3
+        
         if nBetas > 0:
             if del_v is None:
                 v_shaped = self.v_template + torch.reshape(torch.matmul(beta, self.shapedirs[:nBetas,:]), [-1, self.size[0], self.size[1]])
@@ -98,7 +98,7 @@ class SMAL(object):
         pose_feature = torch.reshape(Rs[:, 1:, :, :] - torch.eye(3).cuda(device=self.opts.gpu_id), [-1, 306])
 
 
-        # (N x 207) x (207, 20670) -> N x 6890 x 3
+        
         v_posed = torch.reshape(
             torch.matmul(pose_feature, self.posedirs),
             [-1, self.size[0], self.size[1]]) + v_shaped
@@ -109,11 +109,11 @@ class SMAL(object):
 
         # 5. Do skinning:
         num_batch = theta.shape[0]
-        # W is N x 6890 x 24
+        
         weights_t = self.weights.repeat([num_batch, 1])
         W = torch.reshape(weights_t, [num_batch, -1, 35])
 
-            # (N x 6890 x 24) x (N x 24 x 16)
+            
         T = torch.reshape(
             torch.matmul(W, torch.reshape(A, [num_batch, 35, 16])),
                 [num_batch, -1, 4, 4])
@@ -128,7 +128,7 @@ class SMAL(object):
 
         verts = verts + trans[:,None,:]
 
-        # Get cocoplus or lsp joints:
+        # Get joints:
         joint_x = torch.matmul(verts[:, :, 0], self.J_regressor)
         joint_y = torch.matmul(verts[:, :, 1], self.J_regressor)
         joint_z = torch.matmul(verts[:, :, 2], self.J_regressor)
@@ -148,28 +148,4 @@ class SMAL(object):
 
 
 
-
-def main():
-    import os
-    model_name='my_smpl_00781_4_all.pkl'
-    model_dir = '/Users/silvia/Dropbox/Work/smalr/smpl_models/'
-    model_path = os.path.join(model_dir, model_name)
-    smpl = SMAL(model_path)
-
-    theta = np.zeros((1,105))
-    betas = np.zeros((1,10))
-    theta[0,10] = 0.5
-    betas[0,:] = .5
-    V,J,R = smpl(torch.Tensor(betas), torch.Tensor(theta))
-
-    from psbody.mesh.meshviewer import MeshViewer, MeshViewers
-    from psbody.mesh import Mesh
-
-    Mesh(v=V, f=smpl.f).show()
-
-
-    import pdb; pdb.set_trace()
-
-if __name__ == '__main__':
-    main()
 
